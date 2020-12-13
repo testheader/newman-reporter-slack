a7077aa666c820b9c49130a6c0f364d32cf269ab
testheader <vdlgeert@gmail.com>
2020-12-13 22:41:04 +0100

    Name, assertions, headers and body available

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 module.exports = function NewmanReporterSlackExtended (newman, options) {
-    newman.on('done', (err, e) => {
-      console.log(e)
-      console.log(e.failures)
+
+    let buildup = new Object();
+    buildup.requestName = ""
+    buildup.assertions = []
+    buildup.headers = []
+    buildup.body = ""
+
+    newman.on('beforeItem', (err, e) => {
+        //reset ouput
+        buildup.assertions = []
+        buildup.requestName = e.item.name
     })
 
+    newman.on('assertion', (err, e) => {
+        //set assertion data in object
+        let assertion = new Object()
+        assertion.title = e.assertion;
+        assertion.message = e.error.message;
+
+        //add assertion to list
+        buildup.assertions.push(assertion)
+    })
+
+    newman.on('item', (err, e) => {
+        buildup.requestName = e.item.name
+        buildup.body = e.item.request.body;
+        buildup.headers = e.item.request.headers.members;
+
+
+        //display everything
+        console.log(`\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++`)
+        console.log(`${JSON.stringify(buildup, null, 2)}`)
+    })
 }
