36ed8ec79fce279bb9f04cb9977c5326f7b8b9c6
testheader <vdlgeert@gmail.com>
2020-12-18 23:36:25 +0100

    desired message and basic setup enum

--- enums.js
+++ enums.js
@@ -1,99 +1,67 @@
-const BLOCKS = {
-{
+const BLOCKS = [
+  {
     name: "header",
-    object:
-    {
-        type: "header",
-        text: {
-            type
-        :
-            "plain_text",
-                text
-        :
-            "",
-                emoji
-        :
-            true
-        }
-    }
-}
-,
-{
+    object: {
+      type: "header",
+      text: {
+        type: "plain_text",
+        text: "",
+        emoji: true,
+      },
+    },
+  },
+  {
     name: "summary",
-    object:
-    {
-        type: "section",
-            text
-    :
-        {
-            "type"
-        :
-            "mrkdwn",
-                "text"
-        :
-            ""
-        }
-    }
-}
-,
-{
+    object: {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text:
+          "",
+      },
+    },
+  },
+  {
     name: "subtitle",
-    object:
-    {
-        type: "section",
-            text
-    :
-        {
-            "type"
-        :
-            "mrkdwn",
-                "text"
-        :
-            "*These requests had failing tests:*"
-        }
-    }
-}
-,
-{
+    object: {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: "*These requests had failing tests:*",
+      },
+    },
+  },
+  {
     name: "list",
-    object:
-    {
-        type: "section",
-            text
-    :
-        {
-            "type"
-        :
-            "mrkdwn",
-                "text"
-        :
-            ""
-        }
-    }
-}
-,
-{
+    object: {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: "",
+      },
+    },
+  },
+  {
     name: "investigate",
-    object:
-    {
-        "type": "section",
-        "text": {
-        "type": "mrkdwn",
-            "text": "_Go to run results_"
+    object: {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: "_Go to run results_",
+      },
+      accessory: {
+        type: "button",
+        text: {
+          type: "plain_text",
+          text: "Investigate",
+          emoji: true,
         },
-        "accessory": {
-        "type": "button",
-            "text": {
-            "type": "plain_text",
-                "text": "Investigate",
-                "emoji": true
-        },
-        "value": "investigate",
-            "url": "",
-            "action_id": "button-action"
-    }
-    }
-}
-};
+        value: "investigate",
+        url: "https://google.com",
+        action_id: "button-action",
+      },
+    },
+  },
+];
 
 module.exports = { BLOCKS };
