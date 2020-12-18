48fc29ed9e067cd05d3e648a84af8ee2c1b0f1a4
testheader <vdlgeert@gmail.com>
2020-12-18 23:18:23 +0100

    More data available

---
+++ enums.js
@@ -0,0 +1,99 @@
+const BLOCKS = {
+{
+    name: "header",
+    object:
+    {
+        type: "header",
+        text: {
+            type
+        :
+            "plain_text",
+                text
+        :
+            "",
+                emoji
+        :
+            true
+        }
+    }
+}
+,
+{
+    name: "summary",
+    object:
+    {
+        type: "section",
+            text
+    :
+        {
+            "type"
+        :
+            "mrkdwn",
+                "text"
+        :
+            ""
+        }
+    }
+}
+,
+{
+    name: "subtitle",
+    object:
+    {
+        type: "section",
+            text
+    :
+        {
+            "type"
+        :
+            "mrkdwn",
+                "text"
+        :
+            "*These requests had failing tests:*"
+        }
+    }
+}
+,
+{
+    name: "list",
+    object:
+    {
+        type: "section",
+            text
+    :
+        {
+            "type"
+        :
+            "mrkdwn",
+                "text"
+        :
+            ""
+        }
+    }
+}
+,
+{
+    name: "investigate",
+    object:
+    {
+        "type": "section",
+        "text": {
+        "type": "mrkdwn",
+            "text": "_Go to run results_"
+        },
+        "accessory": {
+        "type": "button",
+            "text": {
+            "type": "plain_text",
+                "text": "Investigate",
+                "emoji": true
+        },
+        "value": "investigate",
+            "url": "",
+            "action_id": "button-action"
+    }
+    }
+}
+};
+
+module.exports = { BLOCKS };
--- index.js
+++ index.js
@@ -1,35 +1,55 @@
-module.exports = function NewmanReporterSlackExtended (newman, options) {
+module.exports = function NewmanReporterSlackExtended(newman, options) {
+  let requestInformation;
+  let runInformation;
 
-    let buildup = new Object();
-    buildup.requestName = ""
-    buildup.assertions = []
-    buildup.headers = []
-    buildup.body = ""
+  newman.on("beforeIteration", (err, e) => {
+    //reset and prime run
+    runInformation = new Object();
+    runInformation.requests = [];
+    runInformation.totalFailedRequest = 0;
+    runInformation.totalFailedAssertions = 0;
+    runInformation.collection = newman.summary.collection.name;
+  });
 
-    newman.on('beforeItem', (err, e) => {
-        //reset ouput
-        buildup.assertions = []
-        buildup.requestName = e.item.name
-    })
+  newman.on("beforeItem", (err, e) => {
+    //reset and prime request
+    requestInformation = new Object();
+    requestInformation.requestName = "";
+    requestInformation.assertions = [];
+    requestInformation.headers = [];
+    requestInformation.body = "";
+    requestInformation.failedAssertions = 0;
+    requestInformation.requestFailed = false;
+  });
 
-    newman.on('assertion', (err, e) => {
-        //set assertion data in object
-        let assertion = new Object()
-        assertion.title = e.assertion;
-        assertion.message = e.error.message;
+  newman.on("assertion", (err, e) => {
+    //set assertion data in object
+    let assertion = new Object();
+    assertion.title = e.assertion;
+    assertion.message = e.error.message;
 
-        //add assertion to list
-        buildup.assertions.push(assertion)
-    })
+    //add assertion to list
+    requestInformation.assertions.push(assertion);
+    requestInformation.failedAssertions++;
+    runInformation.totalFailedAssertions++;
+    requestInformation.requestFailed = true;
+  });
 
-    newman.on('item', (err, e) => {
-        buildup.requestName = e.item.name
-        buildup.body = e.item.request.body;
-        buildup.headers = e.item.request.headers.members;
+  newman.on("item", (err, e) => {
+    requestInformation.requestName = e.item.name;
+    requestInformation.body = e.item.request.body;
+    requestInformation.headers = e.item.request.headers.members;
 
+    if (requestInformation.requestFailed) {
+      runInformation.totalFailedRequest++;
+    }
 
-        //display everything
-        console.log(`\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++`)
-        console.log(`${JSON.stringify(buildup, null, 2)}`)
-    })
-}
+    //add finished information to the run results
+    runInformation.requests.push(requestInformation);
+  });
+
+  newman.on("done", (err, e) => {
+    //display everything
+    console.log(`${JSON.stringify(runInformation, null, 2)}`);
+  });
+};
